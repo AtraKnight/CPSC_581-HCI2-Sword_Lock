bf98089cd4bfcb2dc6e01e832539c1900cea3e62
Hardeep <kalersh@gmail.com>
2021-10-27 03:32:27 -0600

    changed swords. adjusted colors

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,10 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -26,6 +31,8 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View.OnTouchListener;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -60,8 +67,6 @@ public class MainActivity extends AppCompatActivity {
     boolean drawn;
 
 
-
-
     String msg;
     private android.widget.RelativeLayout.LayoutParams layoutParams;
 
@@ -74,39 +79,47 @@ public class MainActivity extends AppCompatActivity {
         entry = 0;
         drawn = false;
         onStart(savedInstanceState);
+        f1=(ImageView)findViewById(R.id.glyph1);
+        f2=(ImageView)findViewById(R.id.glyph2);
+        f3=(ImageView)findViewById(R.id.glyph3);
+        f4=(ImageView)findViewById(R.id.glyph4);
+        f5=(ImageView)findViewById(R.id.glyph5);
+        f6=(ImageView)findViewById(R.id.glyph6);
+
+
+
     }
     void enterPassword(int i){
 
         if(entry<6) {
             password[entry]=i;
             entry++;
-            if(entry == 6){
-                validatePassword(password);
-            }
+            drawn = true;
         }
     }
-    void validatePassword(int [] p){
+    boolean validatePassword(){
 
 
         if(Arrays.equals(code,password)){
             //do if true
-            System.exit(0);
+            return true;
         }
         else{
             // do if false
-            g1=(ImageView)findViewById(R.id.oGlyph1);
-            g2=(ImageView)findViewById(R.id.oGlyph2);
-            g3=(ImageView)findViewById(R.id.oGlyph3);
-            g4=(ImageView)findViewById(R.id.oGlyph4);
-            g5=(ImageView)findViewById(R.id.oGlyph5);
-            g6=(ImageView)findViewById(R.id.oGlyph6);
             entry = 0;
+            f1.setAlpha(0.f);
+            f2.setAlpha(0.f);
+            f3.setAlpha(0.f);
+            f4.setAlpha(0.f);
+            f5.setAlpha(0.f);
+            f6.setAlpha(0.f);
             g1.setVisibility(View.VISIBLE);
             g2.setVisibility(View.VISIBLE);
             g3.setVisibility(View.VISIBLE);
             g4.setVisibility(View.VISIBLE);
             g5.setVisibility(View.VISIBLE);
             g6.setVisibility(View.VISIBLE);
+            return false;
         }
     }
     protected void onStart(Bundle savedInstanceState){
@@ -121,8 +134,27 @@ public class MainActivity extends AppCompatActivity {
         g5=(ImageView)findViewById(R.id.oGlyph5);
         g6=(ImageView)findViewById(R.id.oGlyph6);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.forest_sound);
-        mediaPlayer.start();
+        SoundPool sounds;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            AudioAttributes aA = new AudioAttributes.Builder()
+                    .build();
+            sounds = new SoundPool.Builder().setAudioAttributes(aA).setMaxStreams(6).build();
+        }
+        else {
+
+            sounds = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+        }
+
+
+        int pull = sounds.load(this,R.raw.pull2,1);
+        int forest = sounds.load(this,R.raw.forest,1);
+        int glow = sounds.load(this,R.raw.glow,1);
+
+        long glowdur = 1000;
+
+
 
 
         /* REMOVE this and REPLACE with Swipe Guestures
@@ -220,37 +252,80 @@ public class MainActivity extends AppCompatActivity {
 
         img.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this){
             public void onSwipeTop(){
-                password = new int[]{0,0,0,0,0,0};
-                Log.d("myTag", "num "+Float.toString(g1.getY()));
-                g1.animate().translationY(0).setDuration(500);
-                g2.animate().translationY(0).setDuration(500);
-                g3.animate().translationY(0).setDuration(500);
-                g4.animate().translationY(0).setDuration(500);
-                g5.animate().translationY(0).setDuration(500);
-                g6.animate().translationY(0).setDuration(500);
-                Log.d("myTag", "num "+Float.toString(g1.getY()));
-                img.animate().translationY(img2.getY()-900).setDuration(500).setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        super.onAnimationEnd(animation);
-                        //when first animation ends, start the next one
-                        //Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                        //startActivity(intent);
-                        g1.setClickable(true);
-                        g2.setClickable(true);
-                        g3.setClickable(true);
-                        g4.setClickable(true);
-                        g5.setClickable(true);
-                        g6.setClickable(true);
+                if(drawn ==false) {
+                    password = new int[]{0, 0, 0, 0, 0, 0};
+                    Log.d("myTag", "num " + Float.toString(g1.getY()));
+                    g1.animate().translationY(0).setDuration(500);
+                    g2.animate().translationY(0).setDuration(500);
+                    g3.animate().translationY(0).setDuration(500);
+                    g4.animate().translationY(0).setDuration(500);
+                    g5.animate().translationY(0).setDuration(500);
+                    g6.animate().translationY(0).setDuration(500);
+                    Log.d("myTag", "num " + Float.toString(g1.getY()));
+                    img.animate().translationY(img2.getY() - 1000).setDuration(500).setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            //when first animation ends, start the next one
+                            //Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                            //startActivity(intent);
+                            g1.setClickable(true);
+                            g2.setClickable(true);
+                            g3.setClickable(true);
+                            g4.setClickable(true);
+                            g5.setClickable(true);
+                            g6.setClickable(true);
 
+
+                        }
+                    });
+                    Log.d("myTag", "num " + Float.toString(img2.getY() - 900));
+                }
+                if (drawn ==true){
+                    g1.setVisibility(View.INVISIBLE);
+                    g2.setVisibility(View.INVISIBLE);
+                    g3.setVisibility(View.INVISIBLE);
+                    g4.setVisibility(View.INVISIBLE);
+                    g5.setVisibility(View.INVISIBLE);
+                    g6.setVisibility(View.INVISIBLE);
+                    if(validatePassword()){
+                        sounds.play(pull,1,1,0,0,1);
+                        img.animate().translationY(-3300).setDuration(1000);
+                        f1.animate().translationY(-3300).setDuration(1000);
+                        f2.animate().translationY(-3300).setDuration(1000);
+                        f3.animate().translationY(-3300).setDuration(1000);
+                        f4.animate().translationY(-3300).setDuration(1000);
+                        f5.animate().translationY(-3300).setDuration(1000);
+                        f6.animate().translationY(-3300).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+
+                            }
+                        });
+
+                    }else{
+                        drawn = false;
 
                     }
-                });
-                Log.d("myTag", "num "+Float.toString(img2.getY()-900));
+
+                }
 
 
             }
             public void onSwipeBottom(){
+                f1.setAlpha(0.f);
+                f2.setAlpha(0.f);
+                f3.setAlpha(0.f);
+                f4.setAlpha(0.f);
+                f5.setAlpha(0.f);
+                f6.setAlpha(0.f);
+                g1.setVisibility(View.VISIBLE);
+                g2.setVisibility(View.VISIBLE);
+                g3.setVisibility(View.VISIBLE);
+                g4.setVisibility(View.VISIBLE);
+                g5.setVisibility(View.VISIBLE);
+                g6.setVisibility(View.VISIBLE);
                 g1.setClickable(false);
                 g2.setClickable(false);
                 g3.setClickable(false);
@@ -264,65 +339,63 @@ public class MainActivity extends AppCompatActivity {
                 g5.animate().translationY(900).setDuration(500);
                 g6.animate().translationY(900).setDuration(500);
                 img.animate().translationY(img2.getY()).setDuration(500);
-                Log.d("myTag", "num "+Float.toString(img2.getY()));
+                Log.d("myTag", "bottom: "+Float.toString(img2.getY()));
                 drawn = false;
                 entry = 0;
+
 
             }
         });
 
 
+
+
             g1.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-
-                    g1.setClickable(false);
-                    g1.setVisibility(View.INVISIBLE);
+                    sounds.play(glow,1,1,0,0,1);
+                    f1.animate().alpha(1f).setDuration(glowdur);
                     enterPassword(1);
-                    f1=(ImageView)findViewById(R.id.glyph1);
-                    f1.setVisibility(View.VISIBLE);
 
                 }
             });
             g2.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    g2.setVisibility(View.INVISIBLE);
+                    sounds.play(glow,1,1,0,0,1);
+                    f2.animate().alpha(1f).setDuration(glowdur);
                     enterPassword(2);
-                    f1=(ImageView)findViewById(R.id.glyph2);
-                    f1.setVisibility(View.VISIBLE);
                 }
             });
             g3.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    g3.setVisibility(View.INVISIBLE);
+                    sounds.play(glow,1,1,0,0,1);
+
+                    f3.animate().alpha(1f).setDuration(glowdur);
                     enterPassword(3);
-                    f1=(ImageView)findViewById(R.id.glyph3);
-                    f1.setVisibility(View.VISIBLE);
                 }
             });
             g4.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    g4.setVisibility(View.INVISIBLE);
+                    sounds.play(glow,1,1,0,0,1);
+                    f4.animate().alpha(1f).setDuration(glowdur);
                     enterPassword(4);
-                    f1=(ImageView)findViewById(R.id.glyph4);
-                    f1.setVisibility(View.VISIBLE);
                 }
             });
             g5.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    g5.setVisibility(View.INVISIBLE);
+                    sounds.play(glow,1,1,0,0,1);
+                    f5.animate().alpha(1f).setDuration(glowdur);
                     enterPassword(5);
-                    f1=(ImageView)findViewById(R.id.glyph5);
-                    f1.animate().
                 }
             });
             g6.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    g6.setVisibility(View.INVISIBLE);
+                    sounds.play(glow,1,1,0,0,1);
+                    f6.animate().alpha(1f).setDuration(glowdur);
                     enterPassword(6);
-                    f1=(ImageView)findViewById(R.id.glyph6);
-                    f1.setVisibility(View.VISIBLE);
                 }
             });
+
+
 
     }
 }
